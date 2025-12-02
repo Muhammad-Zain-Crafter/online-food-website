fc3098374e482b6b99f4a0e32de41d5a6473608d
Muhammad-Zain-Crafter <zmunawar872@gmail.com>
2025-12-02 11:01:48 -0800

    Refactor code structure for improved readability and maintainability

--- basic.js
+++ basic.js
@@ -1,7 +1,30 @@
 
 const menuBtn = document.querySelector(".menu-btn");
 const navLinks = document.querySelector(".nav-links");
+const toggle_btn = document.querySelector(".toggle-btn");
+
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.2 });
+
+cards.forEach(card => {
+    observer.observe(card);
+});
+
+
+
+
+toggle_btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 });
 
